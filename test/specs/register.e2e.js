f2dbfca366157ba608986723ae5e3c652cccf4de
Arthur Naert <arthur.naert@brightest.be>
2025-09-04 15:28:23 +0200

    Add negative flow register feature

--- test/specs/register.e2e.js
+++ test/specs/register.e2e.js
@@ -1,24 +1,54 @@
 const RegisterPage = require("../pageobjects/RegisterPage");
-const { $, browser, expect } = require("@wdio/globals");
+const { browser, expect } = require("@wdio/globals");
 
 describe("Register feature", () => {
-  const timestamp = Date.now();
-  const validEmail = `arthur${timestamp}@brightest.be`;
-  const validPassword = "Test12345!";
+  const basePassword = "Test12345!";
+
+  beforeEach(async () => {
+    await browser.reloadSession();
+  });
 
   it("Register works with correct data", async () => {
+    const timestamp = Date.now();
+    const email = `arthur${timestamp}@brightest.be`;
+
     await RegisterPage.open();
-    await browser.pause(1500);
     await RegisterPage.register(
       "Arthur",
       "Naert",
-      validEmail,
-      validPassword,
-      validPassword
+      email,
+      basePassword,
+      basePassword
     );
-    await browser.pause(1500);
     await expect(browser).toHaveUrl(
       "https://brightest-portal-staging.herokuapp.com/"
+    );
+  });
+
+  it("Show validation errors when submitting empty form or field", async () => {
+    await RegisterPage.open();
+    await RegisterPage.signInButton.click();
+
+    const errors = await RegisterPage.validationErrors;
+    expect(errors.length).toBeGreaterThan(0);
+
+    await expect(errors[0]).toHaveText("No, you missed this one.");
+  });
+
+  it("Show server-side error when password is too short", async () => {
+    const timestamp = Date.now();
+    const shortEmail = `short${timestamp}@brightest.be`;
+
+    await RegisterPage.open();
+    await RegisterPage.register("Arthur", "Naert", shortEmail, "abc", "abc");
+
+    await expect(RegisterPage.serverErrorMessages).toBeElementsArrayOfSize({
+      gte: 1,
+    });
+
+    const errors = await RegisterPage.serverErrorMessages;
+    await expect(errors[0]).toHaveText(
+      "Password is too short (minimum is 8 characters)"
     );
   });
 });

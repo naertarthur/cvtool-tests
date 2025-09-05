c5dcf2774a5b37f2aa8ba8b6169a6353244f80fc
Arthur Naert <arthur.naert@brightest.be>
2025-09-05 08:51:38 +0200

    Add login tests

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,11 +1,30 @@
-const LoginPage = require('../pageobjects/LoginPage');
-const { $, browser, expect } = require('@wdio/globals');
+const LoginPage = require("../pageobjects/LoginPage");
+const { browser, expect } = require("@wdio/globals");
 
-describe('Login feature', () => {
-    
-    it('Login works with valid credentials', async () => {
-        
+it("Show an error for invalid password", async () => {
+  await LoginPage.open();
 
-    });
+  await LoginPage.login("arthur.naert@brightest.be", "fake");
+  await browser.pause(1500);
+  await expect(LoginPage.errorMessage).toBeDisplayed();
+  await expect(LoginPage.errorMessage).toHaveText("Invalid Email or password.");
+});
 
+it("Show an error for invalid email", async () => {
+  await LoginPage.open();
+
+  await LoginPage.login("fake@brightest.be", "Test12345!");
+  await browser.pause(1500);
+
+  await expect(LoginPage.errorMessage).toBeDisplayed();
+  await expect(LoginPage.errorMessage).toHaveText("Invalid Email or password.");
+});
+it("Login works with valid credentials", async () => {
+  await LoginPage.open();
+
+  await LoginPage.login("johndoe1@brightest.be", "Test12345!");
+  await browser.pause(1500);
+  await expect(browser).toHaveUrl(
+    "https://brightest-portal-staging.herokuapp.com/"
+  );
 });
